e33606d572a9b69d38bcf58ac21910cd84360e82
Thomas Hartmann <thomas@webtrade.dk>
2026-08-20 13:17:38 +0200

    latest

--- src/test/java/dk/ek/persistence/StudentDAOTest.java
+++ src/test/java/dk/ek/persistence/StudentDAOTest.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.*;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,12 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentDAOTest {
     private static EntityManagerFactory emf;
     private static IDAO<Student> studentDAO;
+    private static IRetrieveCourseData retrieveCourseData;
     private Student s1, s2, s3;
+    private Course c1, c2;
 
     @BeforeAll
     static void beforeAll(){
         emf = HibernateConfig.getEntityManagerFactoryForTest();
         studentDAO = new StudentDAO(emf);
+        retrieveCourseData = new CourseDAO(emf);
+
     }
 
     @BeforeEach
@@ -24,9 +29,21 @@ class StudentDAOTest {
         s1 = new Student("Holger", "ho@mail.com");
         s2 = new Student("Henriette", "he@mail.com");
         s3 = new Student("Hassan", "ha@mail.com");
+        LocalDate start = LocalDate.of(2026, 2, 1);
+        LocalDate end = start.plusMonths(6);
+        c1 = new Course("MATH", "Mathematics course", start, end);
+        c2 = new Course("ART", "Art course", start, end);
+
         try(EntityManager em = emf.createEntityManager()){
             em.getTransaction().begin();
             em.createQuery("DELETE FROM Student s").executeUpdate();
+            em.persist(c1);
+            em.persist(c2);
+            em.getTransaction().commit();
+
+            em.getTransaction().begin();
+            s1.assignToCourse(c1.getId());
+            s2.assignToCourse(c1.getId());
             em.persist(s1);
             em.persist(s2);
             em.persist(s3);
@@ -51,5 +68,19 @@ class StudentDAOTest {
         Set<Student> students = studentDAO.get();
         System.out.println("Student SIZE: "+students.size());
         assertTrue(students.size()==3);
+    }
+
+    @Test
+    @DisplayName("Test assignment by getting students from specific course")
+    void assignmentTest(){
+        Set<Student> studentsInCourse1 = retrieveCourseData.getByCourseId(c1.getId());
+        assertTrue(studentsInCourse1.size() == 2);
+
+    }
+    @Test
+    @DisplayName("Test courses by a students")
+    void assignmentTest2(){
+        Set<Course> coursesByStudent2 = retrieveCourseData.getCoursesByStudent(s2.getId());
+        assertTrue(coursesByStudent2.size() == 1);
     }
 }
